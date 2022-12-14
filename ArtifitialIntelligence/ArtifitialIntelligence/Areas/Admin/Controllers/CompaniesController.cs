e108ebb45833bfcf4117c245787756a220669132
imran <imranbsmrstucse@gmail.com>
2022-12-14 13:21:57 +0600

    Companies Controller Related Creae Method Added

--- ArtifitialIntelligence/ArtifitialIntelligence/Areas/Admin/Controllers/CompaniesController.cs
+++ ArtifitialIntelligence/ArtifitialIntelligence/Areas/Admin/Controllers/CompaniesController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ArtifitialIntelligence.Data;
+using ArtifitialIntelligence.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ArtifitialIntelligence.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CompaniesController : Controller
     {
+
+        ApplicationDbContext _context;
+        public CompaniesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var companies = _context.Companies.ToList();
+            return View(companies);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Company company)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Companies.Add(company);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(company);
         }
     }
 }
