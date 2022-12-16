40f6e6487f5ab7dffa09a319758e3f9ec49c30f2
imran <imranbsmrstucse@gmail.com>
2022-12-17 01:34:27 +0600

    Company Home Details Added

--- ArtifitialIntelligence/ArtifitialIntelligence/Areas/Customer/Controllers/HomeController.cs
+++ ArtifitialIntelligence/ArtifitialIntelligence/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using ArtifitialIntelligence.Models;
+﻿using ArtifitialIntelligence.Data;
+using ArtifitialIntelligence.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using OnlineShop.Utility;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,18 +15,63 @@ namespace ArtifitialIntelligence.Controllers
     [Area("Customer")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        ApplicationDbContext _context;
+
+
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context=context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var companies=_context.Companies.ToList();
+            return View(companies);
         }
 
+
+
+        //Get Product Details Action Method
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var company = _context.Companies.FirstOrDefault(c => c.Id == id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+            return View(company);
+        }
+        //Post Product Details Action in Session Method
+        [HttpPost]
+        [ActionName("Details")]
+        public ActionResult ProductDetails(int? id)
+        {
+            List<Company> companies = new List<Company>();
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var company = _context.Companies.FirstOrDefault(c => c.Id == id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+            companies = HttpContext.Session.Get<List<Company>>("companies");
+            if (companies == null)
+            {
+                companies = new List<Company>();
+            }
+            companies.Add(company);
+            HttpContext.Session.Set("companies", companies);
+            //return View(product);
+            return RedirectToAction(nameof(Index));
+        }
         public IActionResult Privacy()
         {
             return View();
