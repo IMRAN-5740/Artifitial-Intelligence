678c9eed91fc6d23f40dd1daa8469aa4a51470a7
imran <imranbsmrstucse@gmail.com>
2022-12-17 16:26:26 +0600

    Add Identity

---
+++ ArtifitialIntelligence/ArtifitialIntelligence/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ArtifitialIntelligence.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ArtifitialIntelligence.Areas.Identity.IdentityHostingStartup))]
+namespace ArtifitialIntelligence.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
