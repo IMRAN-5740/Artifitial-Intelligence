588ffaa339fddb7d0ee1b3bca7eba311c8e6d1b5
imran <imranbsmrstucse@gmail.com>
2022-12-18 14:34:55 +0600

    Create User Set

--- ArtifitialIntelligence/ArtifitialIntelligence/Areas/Customer/Controllers/UserController.cs
+++ ArtifitialIntelligence/ArtifitialIntelligence/Areas/Customer/Controllers/UserController.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using ArtifitialIntelligence.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System.Threading.Tasks;
 
 namespace ArtifitialIntelligence.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class UserController : Controller
     {
 
@@ -22,6 +26,25 @@ namespace ArtifitialIntelligence.Areas.Customer.Controllers
         public async Task<IActionResult> Create()
         {
 
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(ApplicationUser user)
+        {
+            if(ModelState.IsValid)
+            {
+                var userResult = await _userManager.CreateAsync(user, user.PasswordHash);
+                if (userResult.Succeeded)
+                {
+                    TempData["Save"] = "Registration Successfull";
+                    return RedirectToAction(nameof(Index));
+                }
+                foreach (var error in userResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            
             return View();
         }
     }
